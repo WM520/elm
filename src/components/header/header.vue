6a706f1e5262cf93405662c4432d5e6f585673c4
汪淼 <1071597946@qq.com>
2017-04-25 14:59:27 +0800

    star组件开发

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -21,26 +21,52 @@
 					<span class=""></span>
 				</div>
 			</div>
-			
 		</div>
-		<div class="bulletin-wrapper">
+		<div class="bulletin-wrapper" @click="showDetail">
 				<span class="bulletin-title"><span></span></span>
 				<span class="bulletin-text">{{ seller.bulletin }}</span>
 		</div>
 		<div class="background">
 			<img :src="seller.avatar" width="100%" height="100%">
+			<!-- <img :src="seller.avatar" width="100%" height="100%"> -->
+		</div>
+		<div v-show="detailShow" class="detail">
+			<div class="detail-wrapper clearfix">
+				<div class="detail-main">
+					<h1 class="name">{{ seller.name }}</h1>
+					<star :size="48" :score="seller.score"></star>
+				</div>
+			</div>
+			<div class="detail-close">
+				<i class="icon-close"></i>
+			</div>
+
 		</div>
 	</div>
 </template>
 <script type="text/javascript">
+import Star from '@/components/star/star.vue'
 	export default {
 		props: {
 			seller: {
 				type: Object
 			}
 		},
+		data() {
+			return {
+				detailShow: false
+			}
+		},
 		created() {
 			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+		},
+		methods: {
+			showDetail() {
+				this.detailShow = true
+			}
+		},
+		components: {
+			Star
 		}
 	}
 </script>
@@ -49,6 +75,7 @@
 .header
 	position: relative
 	color: #fff
+	overflow: hidden
 	background: rgba(7,17,27,0.2)
 	.content-wrapper
 		position: relative
@@ -146,6 +173,33 @@
 		width: 100%
 		height: 100%
 		z-index: -1	
-		filter: blur(10px)	
+		filter: blur(10px)	// 模糊效果
+	.detail
+		position: fixed
+		z-index: 100
+		top: 0
+		left: 0
+		width: 100%
+		height: 100%
+		overflow: auto
+		background: rgba(7,17,27,0.8)
+		.detail-wrapper
+			min-height: 100%
+			width: 100%
+			.detail-main
+				margin-top: 64px
+				padding-bottom: 64px
+				.name
+					line-height: 16px
+					text-align: center
+					font-size: 16px
+					font-weight: 700
+		.detail-close
+			position: relative
+			width: 32px
+			height: 32px
+			margin: -64px auto 0 auto
+			clear: both
+			font-size: 32px
 
 </style>
